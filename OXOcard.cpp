df59de1668b1a8e888ac6984e4626fe63c081ed5
Jascha Haldemann <jh@oxon.ch>
2017-06-07 18:05:33 +0200

    added LGPL-V2.1 license

--- OXOcard.cpp
+++ OXOcard.cpp
@@ -5,6 +5,19 @@
 * \author  Thomas Garaio tg@oxon.ch
 * \date    01.02.2017
 * \version 1.0
+*
+* \license LGPL-V2.1
+* Copyright (c) 2017 OXON AG. All rights reserved.
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, see 'http://www.gnu.org/licenses/'
 *******************************************************************************/
 
 /* ================================= Imports ================================ */
---
+++ README.md
@@ -0,0 +1,1 @@
+This project is licensed under the terms of the GNU Lesser General Public License.
